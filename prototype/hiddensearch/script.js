e15209cf506deaa6bc2c60b977ce34ac539e6e71
Antony.C <93124252+CodingBDX@users.noreply.github.com>
2022-01-29 15:12:59 +0100

    Add files via upload

---
+++ prototype/hiddensearch/script.js
@@ -0,0 +1,10 @@
+
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+
+btn.addEventListener('click', () => {
+    search.classList.add('active')
+    input.focus()
+})
